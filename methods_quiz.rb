754018196c42b6959c5098bbfa137e44b2e23069
Keelin Hunt <huntkd@s.dcsdk12.org>
2017-10-02 10:42:02 -0600

    Implement has_teen?

--- methods_quiz.rb
+++ methods_quiz.rb
@@ -1,24 +1,21 @@
   
 # TODO - write has_teen?
 
-def has_teen? (n1, n2, n3)
+def has_teen?(int1, int2, int3)
 
-	if n1 == 13 || n1 == 14 || n1 == 15 || n1 == 16 || n1 == 17 || n1 == 18 || n1 == 19
+	if int1 == 13 || int1 == 14 || int1 == 15 || int1 == 16 || int1 == 17 || int1 == 18 || int1 == 19
 		return true
 	end
-
-	if n2 == 13 || n2 == 14 || n2 == 15 || n2 == 16 || n2 == 17 || n2 == 18 || n2 == 19
+	if int2 == 13 || int2 == 14 || int2 == 15 || int2 == 16 || int2 == 17 || int2 == 18 || int2 == 19
 		return true
 	end
-
-	if n3 == 13 || n3 == 14 || n3 == 15 || n3 == 16 || n3 == 17 || n3 == 18 || n3 == 19
+	if int3 == 13 || int3 == 14 || int3 == 15 || int3 == 16 || int3 == 17 || int3 == 18 || int3 == 19
 		return true
 	end
 
 	return false
 
 end
-
 
 # TODO - write not_string
 
