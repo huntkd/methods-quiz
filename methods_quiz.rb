039b5f9a24056d16d7dadd72d3073657160e0b98
Keelin Hunt <huntkd@s.dcsdk12.org>
2017-10-02 10:36:04 -0600

    Tests for has_teen?

--- methods_quiz.rb
+++ methods_quiz.rb
@@ -1,6 +1,25 @@
   
 # TODO - write has_teen?
 
+def has_teen? (n1, n2, n3)
+
+	if n1 == 13 || n1 == 14 || n1 == 15 || n1 == 16 || n1 == 17 || n1 == 18 || n1 == 19
+		return true
+	end
+
+	if n2 == 13 || n2 == 14 || n2 == 15 || n2 == 16 || n2 == 17 || n2 == 18 || n2 == 19
+		return true
+	end
+
+	if n3 == 13 || n3 == 14 || n3 == 15 || n3 == 16 || n3 == 17 || n3 == 18 || n3 == 19
+		return true
+	end
+
+	return false
+
+end
+
+
 # TODO - write not_string
 
 # TODO - write icy_hot?
--- test.rb
+++ test.rb
@@ -3,3 +3,26 @@ require './methods_quiz'
 
 # TODO - write tests here
 
+describe 'Methods' do
+
+  describe 'has_teen?' do
+
+    it 'first number is a teen' do
+      has_teen?(15, 7, 8).must_equal(true)
+    end
+
+    it 'second number is a teen' do
+      has_teen?(1, 18, 9).must_equal(true)
+    end
+
+    it 'third number is a teen' do
+      has_teen?(2, 9, 17).must_equal(true)
+    end
+
+    it 'no number in the teens' do
+      has_teen?(5, 6, 7).must_equal(false)
+    end
+
+  end
+
+end
